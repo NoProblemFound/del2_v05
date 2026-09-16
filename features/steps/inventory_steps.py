dbb4b2d38b46fd768292c8394eb767e567b33411
NoProblemFound <robinpan1@yahoo.com>
2026-09-16 20:47:34 +0200

    Rättat till indexering i @then och gjort koden mer generisk.

--- features/steps/inventory_steps.py
+++ features/steps/inventory_steps.py
@@ -6,27 +6,35 @@ from src.stock_item import StockItem
 def step_impl(context):
     context.stock = Stock() # Skapa ett Stock objekt
 
-@when(u'Lägger till en produkt "glas" med antal 6')
-def step_impl(context):
-    product = StockItem("glas",6) # Skapa ett StockItem objekt "glas"
+@when(u'Lägger till en produkt "{name}" med antal {amount:d}')
+def step_impl(context, name, amount):
+    product = StockItem(name, amount) # Skapa ett StockItem objekt "glas"
     context.stock.add_product(product) # Lägger till glas i item[]
 
-@then(u'Lagret ska innehålla en produkt med namnet glas och antalet skall vara 6 st')
-def step_impl(context):
-    assert context.stock.items[0].name == "glas"
-    assert context.stock.items[0].amount == 6
+@then(u'Lagret ska innehålla en produkt med namnet {name} och antalet skall vara {amount:d} st')
+def step_impl(context, name, amount):
+    for item in context.stock.items: # Itererar genom items[]
+        if item.name == name: # Hittat ett matchande namn på produkten
+            assert item.amount == amount
+            return # Avbryter loppen
+    assert False, f"Produkten {name} finns inte i lagret" # Produkten finns inte i listan.
 
 
-@given(u'Lagret har 6 st "glas"')
-def step_impl(context):
+@given(u'Lagret har {amount:d} st {name}')
+def step_impl(context,amount,name):
     context.stock = Stock()
-    product = StockItem("glas",6)
+    product = StockItem(name, amount)
     context.stock.add_product(product)
 
-@when(u'Minskar antalet av "glas" med 2')
-def step_impl(context):
-    context.stock.decrease_product("glas", 2)
+@when(u'Minskar antalet av {name} med {amount:d}')
+def step_impl(context,name,amount):
+    context.stock.decrease_product(name, amount)
 
-@then(u'Det finns kvar 4 "glas" kvar på lagret.')
-def step_impl(context):
-    assert context.stock.items[0].amount == 4
+
+@then(u'Det finns kvar {amount:d} {name} kvar på lagret.')
+def step_impl(context, amount, name):
+    for item in context.stock.items:
+        if item.name == name:
+            assert item.amount == amount
+            return
+    assert False, f"Produkten {name} finns inte i lagret"
